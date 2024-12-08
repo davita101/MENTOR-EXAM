a87f93d290e2150ddda09a6e4202bfc66a0cfb9c
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2024-12-08 09:31:53 +0400

    commit

---
+++ 1.py
@@ -0,0 +1,31 @@
+# Input: ("listen", "silent") → Output: True
+# 2. Input: ("triangle", "integral") → Output: True
+# 3. Input: ("apple", "pale") → Output: False
+# 4. Input: ("a", "a") → Output: True
+# 5. Input: ("rat", "car") → Output: False
+
+
+
+def splitter(a):
+    return [i for i in a]
+
+def funk(a,b):
+    _a = splitter(a)
+    _b = splitter(b)
+    _new = []
+    for char in _a:
+        if char in _b:
+            _new.append(char)
+            _b.remove(char)
+    RES = len(_new) == len(_a)
+    return RES
+
+print(funk("listen", "silent"))
+print(funk("triangle", "integral"))
+print(funk("apple", "pale"))
+print(funk("a", "a"))
+print(funk("rat", "car"))
+print(funk("ll", "ll"))
+print(funk("lp", "ll"))
+print(funk("rrrr", "rrrs"))
+print(funk("rrrr", "rrrr"))
